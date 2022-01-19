d33c2b3e2fd65aeb717eec02538f2820f9195fbc
Vijeeth143 <kuttyk321@gmail.com>
2022-01-19 14:55:16 +0530

    fsd

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Maintain from './componets/Maintain';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Maintain/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componets/Maintain.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { Container,Row } from 'react-bootstrap'
+import './Main.css'
+import fix from "./videos/fix.mp4"
+import me from "./img/me.jpg"
+import git from "./icons/git.svg"
+import twitter from "./icons/twitter.svg"
+import instagram from "./icons/instagram.svg"
+import linkedin from "./icons/linkedin.svg"
+import laptop from "./icons/website.png"
+
+export default class Maintain extends Component {
+    render() {
+
+        return (
+            <div>
+                <Container fluid id="main">              
+                <video autoPlay loop muted src={fix} type='video/mp4' />
+                    
+                    <Row id='ssss'>
+                        <div id="imag">
+                            <img id='img' src={me} alt="" />
+                        </div>
+                        <div id='name'>
+                            Hi, I'm Vijeeth<br></br>
+                            Let's Connect together
+                        </div>
+                        <a id='box' href="https://vijeeth143.github.io/updateweb/">
+                        Portable Website <img src={laptop} alt="" />
+                        </a>
+                        <a id='box' href="https://instagram.com/vijeeth_sankar?utm_medium=copy_link">
+                        Instagram <img src={instagram} alt="" />
+                        </a>
+                        <a id='box' href="https://github.com/Vijeeth143">
+                        GitHub <img src={git} alt="" />
+                        </a>
+                        <a id='box' href="https://www.linkedin.com/in/vijeeth-sankar-2123b1201">
+                        LinkedIn <img src={linkedin} alt="" />
+                        </a>
+                        <a id='box' href="https://twitter.com/Vijeeth_Sankar">
+                        Twitter <img id="twi" src={twitter} alt="" />
+                        </a>
+                        
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+}
