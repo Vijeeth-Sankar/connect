3f662b0f642f80388a16e31e8b73259da3f8cb14
Vijeeth143 <kuttyk321@gmail.com>
2022-01-19 16:15:10 +0530

    update

--- src/componets/Maintain.js
+++ src/componets/Maintain.js
@@ -8,14 +8,16 @@ import twitter from "./icons/twitter.svg"
 import instagram from "./icons/instagram.svg"
 import linkedin from "./icons/linkedin.svg"
 import laptop from "./icons/website.png"
+import whatsapp from "./icons/whatsapp.svg"
 
 export default class Maintain extends Component {
     render() {
 
         return (
             <div>
-                <Container fluid id="main">              
-                <video autoPlay loop muted src={fix} type='video/mp4' />
+                <Container fluid id="main">  
+                            
+                <video autoPlay="" loop="" muted="" playsInline="" src={fix} type='video/mp4' />
                     
                     <Row id='ssss'>
                         <div id="imag">
@@ -37,8 +39,11 @@ export default class Maintain extends Component {
                         <a id='box' href="https://www.linkedin.com/in/vijeeth-sankar-2123b1201">
                         LinkedIn <img src={linkedin} alt="" />
                         </a>
-                        <a id='box' href="https://twitter.com/Vijeeth_Sankar">
+                        <a id='box' className='twi' href="https://twitter.com/Vijeeth_Sankar">
                         Twitter <img id="twi" src={twitter} alt="" />
+                        </a>
+                        <a id='box' className='twi' href="https://twitter.com/Vijeeth_Sankar">
+                        Whatsapp <img id="twi" src={whatsapp} alt="" />
                         </a>
                         
                     </Row>
